79fbd315b8b7eb0455fc269e4229cc353d44728e
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2024-07-23 13:08:13 +0000

    Deploying to gh-pages from @ dmitrmedv/language-school@6f2e24e8a71e996c2ca8868fc61e56f1e285304b 🚀

---
+++ index.db9c8dc6.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".burger-menu"),n=document.querySelector(".close-btn"),c=document.querySelector(".overlay");function t(){c.classList.remove("hidden"),n.addEventListener("click",r)}function r(){c.classList.add("hidden"),n.removeEventListener("click",t)}e.addEventListener("click",t)}();
+//# sourceMappingURL=index.db9c8dc6.js.map
