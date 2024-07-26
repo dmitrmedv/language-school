54f5ca115f64bb8f3a27fe89e5918f29b3ef4b99
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2024-07-26 12:33:14 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,20 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const closeBtn = document.querySelector('.close-btn');
 const overlay = document.querySelector('.overlay');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 burgerMenu.addEventListener('click', showMenu);
 
 function showMenu() {
   overlay.classList.remove('hidden');
   closeBtn.addEventListener('click', hideMenu);
+  mobileMenu.addEventListener('click', hideMenu);
 }
 
-function hideMenu() {
+function hideMenu(e) {
+  if (e.target.classList.contains('link')) {
+    mobileMenu.removeEventListener('click', hideMenu);
+  }
   overlay.classList.add('hidden');
   closeBtn.removeEventListener('click', showMenu);
 }
